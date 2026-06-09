03b4322ae0c9cb3969f71c0defead6549bd79a87
daw_c17 <100421659+dawc17@users.noreply.github.com>
2026-06-09 12:00:38 +0200

    skybox

--- resources/shaders/skybox.fs
+++ resources/shaders/skybox.fs
@@ -8,5 +8,5 @@ out vec4 finalColor;
 
 void main()
 {
-    finalColor = vec4(texture(environmentMap, fragPosition).rgb, 1.0);
+    finalColor = vec4(texture(environmentMap, vec3(fragPosition.x, -fragPosition.y, fragPosition.z)).rgb, 1.0);
 }
